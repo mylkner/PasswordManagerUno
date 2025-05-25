f7d259bacdafcae7297b71f5733cec677281ebdf
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-05-26 00:51:44 +0100

    incomplete ui and logic for adding, deleting, and decrypting passwords

--- PasswordManager/Models/ViewModels/PasswordsModel.cs
+++ PasswordManager/Models/ViewModels/PasswordsModel.cs
@@ -9,4 +9,51 @@ public partial record PasswordsModel(
     private readonly byte[] _encKey = EncryptionKeyService.EncryptionKey;
     public IListState<Password> Passwords =>
         ListState<Password>.Async(this, DBService.GetPasswords);
+
+    public void DecryptPassword(Password password)
+    {
+        try
+        {
+            string decryptedPassword = EncryptionService.DecryptPassword(
+                password.EncryptedPassword,
+                password.Iv,
+                _encKey
+            );
+            Console.WriteLine(decryptedPassword);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public async Task AddPassword(CancellationToken ct)
+    {
+        try
+        {
+            (byte[] encryptedPassword, byte[] iv) = EncryptionService.EncryptPassword(
+                "awd",
+                _encKey
+            );
+            Password newPassword = await DBService.AddPassword("Tile", encryptedPassword, iv);
+            await Passwords.AddAsync(newPassword, ct);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    public async Task DeletePassword(int id, CancellationToken ct)
+    {
+        try
+        {
+            await DBService.DeletePassword(id);
+            await Passwords.RemoveAllAsync(match: pwd => pwd.Id == id, ct);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
--- PasswordManager/Presentation/PasswordsPage.cs
+++ PasswordManager/Presentation/PasswordsPage.cs
@@ -6,18 +6,95 @@ public sealed partial class PasswordsPage : Page
     {
         this.DataContext<PasswordsViewModel>(
             (page, vm) =>
-                page.NavigationCacheMode(NavigationCacheMode.Required)
+                page.Name(out var root)
+                    .NavigationCacheMode(NavigationCacheMode.Required)
                     .Content(
-                        new StackPanel()
-                            .VerticalAlignment(VerticalAlignment.Center)
-                            .HorizontalAlignment(HorizontalAlignment.Center)
-                            .Children(
-                                new ListView()
-                                    .ItemsSource(() => vm.Passwords)
-                                    .ItemTemplate<Password>(pwd =>
-                                        new TextBlock().Text(() => pwd.Title)
-                                    )
-                            )
+                        new ScrollViewer().Content(
+                            new Border()
+                                .SafeArea(SafeArea.InsetMask.VisibleBounds)
+                                .Child(
+                                    new StackPanel()
+                                        .MaxWidth(800)
+                                        .Padding(10, 70)
+                                        .Spacing(20)
+                                        .VerticalAlignment(VerticalAlignment.Center)
+                                        .HorizontalAlignment(HorizontalAlignment.Stretch)
+                                        .Children(
+                                            new Grid()
+                                                .ColumnDefinitions("*, *")
+                                                .ColumnSpacing(10)
+                                                .VerticalAlignment(VerticalAlignment.Center)
+                                                .HorizontalAlignment(HorizontalAlignment.Left)
+                                                .Children(
+                                                    new TextBlock()
+                                                        .VerticalAlignment(VerticalAlignment.Center)
+                                                        .FontSize(20)
+                                                        .Text("Passwords"),
+                                                    new Button()
+                                                        .Grid(row: 0, column: 1)
+                                                        .VerticalAlignment(VerticalAlignment.Center)
+                                                        .Content("+")
+                                                        .Command(() => vm.AddPassword)
+                                                ),
+                                            new ListView()
+                                                .HorizontalAlignment(HorizontalAlignment.Stretch)
+                                                .ItemsSource(() => vm.Passwords)
+                                                .ItemTemplate<Password>(
+                                                    (pwd) =>
+                                                        new Button()
+                                                            .Padding(20)
+                                                            .Margin(0, 0, 0, 20)
+                                                            .Background(
+                                                                new SolidColorBrush(Colors.DarkGray)
+                                                            )
+                                                            .HorizontalAlignment(
+                                                                HorizontalAlignment.Stretch
+                                                            )
+                                                            .Command(x =>
+                                                                x.Source(root)
+                                                                    .DataContext<PasswordsViewModel>()
+                                                                    .Binding(v => v.DecryptPassword)
+                                                            )
+                                                            .CommandParameter(() => pwd)
+                                                            .Content(
+                                                                new Grid()
+                                                                    .ColumnDefinitions("*, *")
+                                                                    .CornerRadius(20)
+                                                                    .Children(
+                                                                        new TextBlock()
+                                                                            .Grid(column: 0)
+                                                                            .HorizontalAlignment(
+                                                                                HorizontalAlignment.Left
+                                                                            )
+                                                                            .VerticalAlignment(
+                                                                                VerticalAlignment.Center
+                                                                            )
+                                                                            .Text(() => pwd.Title),
+                                                                        new Button()
+                                                                            .Grid(column: 1)
+                                                                            .HorizontalAlignment(
+                                                                                HorizontalAlignment.Right
+                                                                            )
+                                                                            .VerticalAlignment(
+                                                                                VerticalAlignment.Center
+                                                                            )
+                                                                            .Content("Delete")
+                                                                            .Command(x =>
+                                                                                x.Source(root)
+                                                                                    .DataContext<PasswordsViewModel>()
+                                                                                    .Binding(v =>
+                                                                                        v.DeletePassword
+                                                                                    )
+                                                                            )
+                                                                            .CommandParameter(
+                                                                                () => pwd.Id
+                                                                            )
+                                                                    )
+                                                            )
+                                                )
+                                        )
+                                )
+                        )
                     )
         );
     }
